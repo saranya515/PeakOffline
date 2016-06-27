80449d8d9d36a7eee61f673a37fdd77d9f22118b
Matthias Gloel <matthiasgloel@Matthiass-MacBook-Air.local>
2016-06-27 17:46:41 +0200

    Initial commit

---
+++ csv2peak.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+
+csv2peak.py 
+
+Input: Traffic and spot schedules as CSV
+
+Output: Spreadsheet with NetUplift Per Spot
+
+Created on Mon Jun 27 17:44:38 2016
+
+@author: matthiasgloel
+"""
+
