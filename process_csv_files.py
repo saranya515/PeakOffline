745ac38c56b51e594a50049e8239fbd8b040f199
Matthias Gloel <matthiasgloel@Matthiass-MacBook-Air.local>
2016-07-04 16:22:58 +0200

    filter march april  works

--- process_csv_files.py
+++ process_csv_files.py
@@ -9,6 +9,8 @@ Process csv files from wix.com
 import os
 import glob
 import pandas as pd
+import datetime as dt
+
 # set path to one of script, keep spoteffects data in same folder
 abspath = os.path.abspath('process_csv_files.py')
 dname = os.path.dirname(abspath)
@@ -34,12 +36,15 @@ df = df.query("fil_ter == 1")
 
 # Drop redundant columns
 df = df.drop(['nul','newsletter','fil_ter', 'channel','url'],1)
-
 df = df.groupby(['date_created'])['anon_count'].sum().reset_index()
+
+df['date_created'] = df.date_created.apply(lambda x: pd.to_datetime(x))
 
 ## Load data of 2016
 afiles = glob.glob("/Users/matthiasgloel/PeakOffline/2016 campaigns/*.csv")
 df2 = pd.concat(pd.read_csv(f, sep=',') for f in afiles) 
+
+test = pd.read_csv("/Users/matthiasgloel/PeakOffline/2015 campaigns/2015-06-03(fr_tv_campaign_without_premiums) copy.csv", sep="\t", parse_dates = True)
 
 # Filter by country, channel and mysterios column H == 1
 df2 = df2.query("country_code == 'DE' ")
@@ -48,8 +53,18 @@ df2 = df2.drop(['device_type', 'channel'], 1)
 
 df2 = df2.groupby(['date_created'])['anon_count'].sum().reset_index()
 
-## merge df and df2, convert time string to datetime
-dF = df.append(df2)
-dF['date_created'] = dF.date_created.apply(lambda x: pd.to_datetime(x))
 
 ### generate missing minutes
+df = generate_missing_minutes(df)
+df2 = generate_missing_minutes(df2)
+
+## merge df and df2, convert time string to datetime
+dF = df.append(df2).reset_index()
+dF['date_created'] = dF.date_created.apply(lambda x: pd.to_datetime(x))
+
+a = pd.tslib.Timestamp('2016-02-15 00:01:00') 
+b = pd.tslib.Timestamp('2016-05-15 23:59:00')
+# filter out month march april
+dF = dF.loc[(dF['date_created'] < a) | (dF['date_created'] > b)]
+
+dF.to_csv('traffic_wix.csv')
